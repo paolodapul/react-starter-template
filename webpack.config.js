edbe6936bdeb8d42b90af0d75f41ca35d6d1a865
Paolo Dapul <markpaolodapul@gmail.com>
2021-08-13 18:23:37 +0800

    Update devServer configuration to follow Webpack 5 spec

--- webpack.config.js
+++ webpack.config.js
@@ -31,10 +31,7 @@ module.exports = {
     filename: "bundle.js",
   },
   devServer: {
-    contentBase: path.join(__dirname, "public/"),
+    static: path.join(__dirname, "public/"),
     port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
 };
