242a173e0bb56832459c7fb278c6b2b995dc0902
Paolo Dapul <markpaolodapul@gmail.com>
2021-08-13 18:27:20 +0800

    Create App component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello!</h1>
+      <p>This is your starter React project.</p>
+    </div>
+  );
+};
+
+export default App;
