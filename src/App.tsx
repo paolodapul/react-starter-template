c3d4963b1de71dff1c1393efa0409badc4d85de0
Paolo Dapul <markpaolodapul@gmail.com>
2021-08-17 14:33:17 +0800

    Change React components to .tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable arrow-body-style */
+import React from "react";
+
+interface Props {}
+
+const App: React.FunctionComponent<Props> = () => {
+  return <div>Hello, React!</div>;
+};
+
+export default App;
